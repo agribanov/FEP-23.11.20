c95e941df56dce065ceec91a370842094ea12a0b
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2021-01-15 17:59:27 +0200

    HW done

--- simple-todo/scripts.js
+++ simple-todo/scripts.js
@@ -1,22 +1,64 @@
+const DONE_CLASS = 'done';
+const DELETE_BTN_CLASS = 'delete-btn';
+const TASK_ITEM_CLASS = 'task-item';
+const TASK_ITEM_SELECTOR = '.' + TASK_ITEM_CLASS;
+
 const taskInput = document.getElementById('taskNameInput');
+const taskTemplate = document.getElementById('newTaskTemplate').innerHTML;
 const listEl = document.getElementById('taskList');
 
-document.getElementById('addTaskBtn').addEventListener('click', onAddBtnClick);
+document.getElementById('newTaskForm').addEventListener('submit', onFormSubmit);
+listEl.addEventListener('click', onListClick);
 
-// addNewTask('Hello world from script');
+function onFormSubmit(e) {
+    e.preventDefault();
 
-function onAddBtnClick() {
-    addNewTask(taskInput.value);
-    clearInput();
+    const title = taskInput.value;
+    if (isValid(title)) {
+        addNewTask(title);
+        clearInput();
+    } else {
+        alert('task is invalid');
+    }
+}
+
+function onListClick(e) {
+    const taskEl = getTaskElement(e.target);
+
+    switch (true) {
+        case e.target.classList.contains(DELETE_BTN_CLASS):
+            return deleteTask(taskEl);
+        case e.target.classList.contains(TASK_ITEM_CLASS):
+            return toggleTask(taskEl);
+    }
+}
+
+function isValid(text) {
+    return !!text;
 }
 
 function addNewTask(title) {
-    const taskEl = document.createElement('div');
-    taskEl.className = 'task-item u-full-width';
-    taskEl.textContent = title;
-    listEl.append(taskEl);
+    const newTaskHtml = getTaskHtml(title);
+
+    listEl.insertAdjacentHTML('beforeend', newTaskHtml);
+}
+
+function getTaskHtml(title) {
+    return taskTemplate.replace('{{text}}', title);
 }
 
 function clearInput() {
     taskInput.value = '';
+}
+
+function getTaskElement(el) {
+    return el.closest(TASK_ITEM_SELECTOR);
+}
+
+function toggleTask(el) {
+    el.classList.toggle(DONE_CLASS);
+}
+
+function deleteTask(el) {
+    el.remove();
 }
