8c95d4b97e5beec774dd179f65cd7d17efcef8c2
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2021-01-28 17:38:49 +0200

    Add tabset

---
+++ tabset/Tabset.js
@@ -0,0 +1,78 @@
+class Tabset {
+    static WRAPPER_CLASS = 'tabset-wrapper';
+    static ELEMENTS_CLASS = 'tabset-element';
+    static ELEMENTS_LIST_CLASS = 'tabset-elements';
+    static TITLES_LIST_CLASS = 'tabset-titles';
+    static TITLE_CLASS = 'tabset-heading';
+    static TITLE_SELECTOR_CLASS = 'title';
+    static BODY_CLASS = 'tabset-body';
+    static BODY_SELECTOR_CLASS = 'body';
+
+    static ACTIVE_CLASS = 'active';
+
+    constructor(container) {
+        this.container = container;
+
+        this.init();
+    }
+
+    init() {
+        this.wrapContainer();
+        this.copyTitles();
+        this.addEventListener();
+        this.show(0);
+    }
+
+    wrapContainer() {
+        this.titlesList = document.createElement('div');
+        this.titlesList.className = Tabset.TITLES_LIST_CLASS;
+
+        const wrapper = document.createElement('div');
+        wrapper.className = Tabset.WRAPPER_CLASS;
+        wrapper.appendChild(this.titlesList);
+
+        this.container.parentNode.insertBefore(wrapper, this.container);
+        wrapper.appendChild(this.container);
+
+        this.container.classList.add(Tabset.ELEMENTS_LIST_CLASS);
+    }
+
+    copyTitles() {
+        Array.prototype.forEach.call(this.container.children, (el) =>
+            el.classList.add(Tabset.ELEMENTS_CLASS)
+        );
+
+        Array.prototype.forEach.call(
+            this.container.querySelectorAll(`.${Tabset.TITLE_SELECTOR_CLASS}`),
+            (el) => {
+                this.titlesList.appendChild(el);
+                el.classList.add(Tabset.TITLE_CLASS);
+            }
+        );
+
+        Array.prototype.forEach.call(
+            this.container.querySelectorAll(`.${Tabset.BODY_SELECTOR_CLASS}`),
+            (el) => el.classList.add(Tabset.BODY_CLASS)
+        );
+    }
+
+    addEventListener() {
+        this.titlesList.addEventListener('click', (e) => this.onTitleClick(e));
+    }
+
+    onTitleClick(e) {
+        // Узнать по кому кликнули и показать соответствующие элементы
+    }
+
+    hideAll() {
+        // Простись по всем показаным элементам и скрыть каждый из них
+    }
+
+    hide(index) {
+        // Спрятать нужные элементы для индекса
+    }
+
+    show(index) {
+        // показать нужные элементы для индекса
+    }
+}
